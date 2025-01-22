0ace118372e4db769390eb9366fbab3bc0741e9f
viklop <viklop7025@gmail.com>
2025-01-22 17:05:05 -0500

    componente finalizado new-page

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-new-page',
-  templateUrl: './new-page.component.html',
-  styleUrl: './new-page.component.css'
+    selector: 'app-new-page',
+    templateUrl: './new-page.component.html',
+    styleUrl: './new-page.component.css',
 })
 export class NewPageComponent {
-
+    public publishers = [
+        {
+            id: 'DC Comics',
+            desc: 'DC - Comics',
+        },
+        {
+            id: 'Marvel Comics',
+            desc: 'Marvel - Comics',
+        },
+    ]
 }
